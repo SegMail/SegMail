1270c9a491219e9e8ed80d1d9ff9636abcbc827e
hareluya86 <hareluya86@hotmail.com>
2014-09-25 00:14:52 +0800

    added phase listener

---
+++ src/main/java/jsf/Seca2PhaseListener.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jsf;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+/**
+ *
+ * @author KH
+ */
+public class Seca2PhaseListener implements PhaseListener {
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+        System.out.println("After phase("+event.getPhaseId()+")");
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        System.out.println("Before phase("+event.getPhaseId()+")");
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+    
+}
