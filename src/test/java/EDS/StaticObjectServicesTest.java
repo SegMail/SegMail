679ab315037483352178980a5cab1614ef8f9da7
hareluya86 <hareluya86@hotmail.com>
2015-08-08 08:49:47 +0800

    Moved MapAPI to EDS

--- src/test/java/EDS/StaticObjectServicesTest.java
+++ src/test/java/EDS/StaticObjectServicesTest.java
@@ -6,6 +6,8 @@
 package EDS;
 
 import eds.component.StaticObjectService;
+import java.util.HashMap;
+import java.util.Map;
 import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
 import javax.naming.NamingException;
@@ -27,7 +29,11 @@ public class StaticObjectServicesTest {
     
     @Test
     public void testContainer() throws NamingException{
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        //EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+        Map<Object, Object> properties = new HashMap<Object, Object>(); 
+        properties.put(EJBContainer.APP_NAME, "SegMail");
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+        EJBContainer container = EJBContainer.createEJBContainer(properties);
         sObjectService = (StaticObjectService)container.getContext().lookup("java:global/classes/StaticObjectService");
         
         Assert.assertNotNull(sObjectService);
