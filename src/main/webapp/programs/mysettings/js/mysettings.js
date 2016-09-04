a8eea43692dead24c18cde30c902f4c2be97d169
hareluya86 <hareluya86@hotmail.com>
2016-09-04 15:01:55 +0800

    Fixed #66

--- src/main/webapp/programs/mysettings/js/mysettings.js
+++ src/main/webapp/programs/mysettings/js/mysettings.js
@@ -1,7 +1,8 @@
 function updateSettings(data) {
     var ajaxstatus = data.status; // Can be "begin", "complete" and "success"
     var block = $(data.source).parents(".block");
-    
+    if(!$('#contact_details_form').validationEngine('validate'))
+        return;
 
     switch (ajaxstatus) {
         case "begin": // This is called right before ajax request is been sent.
